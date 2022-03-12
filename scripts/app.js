c6f678061202c94917982419ca5e7b531f690896
Mayank Goel <goel.mayank235@gmail.com>
2022-03-13 02:20:49 +0530

    Added modal

--- scripts/app.js
+++ scripts/app.js
@@ -317,127 +317,6 @@ async function handleSubmit(event) {
 
 form.addEventListener("submit", handleSubmit);
 
-// // SELECT CAROUSEL
-// const carousel = document.querySelector(".carousel");
-
-// // SELECT NEXT BUTTON
-// const nextButton = document.querySelector(".right-btn");
-
-// // SELECT LEFT BUTTON
-// const previousButton = document.querySelector(".left-btn");
-
-// // SELECT THE NAV
-// const nav = document.querySelector(".carousel-nav");
-
-// // SELECT ALL THE DOTS
-// const dots = [...nav.children];
-
-// // SELECT ALL THE SLIDES INSIDE THE CAROUSEL
-// const slides = [...carousel.children];
-
-// // CALCULATE THE SLIDE WIDTH
-// let slideWidth = slides[0].getBoundingClientRect().width;
-
-// // POSITION THE SLIDES HORIZONTALY
-// function positionSlides(slides) {
-//     for (let index = 0; index < slides.length; index++) {
-//         slides[index].style.left = slideWidth * index + "px";
-//     }
-// }
-
-// positionSlides(slides);
-
-// // ON RIGHT BUTTON CLICK, WE MOVE(TranslateX) THE CAROUSEL TO THE LEFT
-// nextButton.addEventListener("click", function () {
-//     const currentSlide = carousel.querySelector(".active");
-//     const nextSlide = currentSlide.nextElementSibling;
-
-//     moveToSlide(carousel, currentSlide, nextSlide);
-//     hideButton(nextSlide, slides);
-//     moveToDot(nextSlide, slides, nav, dots);
-// });
-
-// // ON LEFT BUTTON CLICK, WE MOVE(TranslateX) THE CAROUSEL TO THE RIGHT
-// previousButton.addEventListener("click", function () {
-//     const currentSlide = carousel.querySelector(".active");
-//     const previousSlide = currentSlide.previousElementSibling;
-
-//     moveToSlide(carousel, currentSlide, previousSlide);
-//     hideButton(previousSlide, slides);
-//     moveToDot(previousSlide, slides, nav, dots);
-// });
-
-// // ON DOT CLICK
-// nav.addEventListener("click", function (e) {
-
-//     // if we didn't click on a dot, we exit
-//     if (e.target === nav) return;
-
-//     // SELECT THE CLICKED DOT
-//     const targetDot = e.target;
-
-//     // SELECT THE CURRENT DOT
-//     const currentDot = nav.querySelector(".active");
-
-//     // SELECT THE CURRENT SLIDE
-//     const currentSlide = carousel.querySelector(".active");
-
-//     // find the index of the dot, so we can target the right slide
-//     let targetDotIndex = findIndex(targetDot, dots);
-
-//     // SELECT THE TARGET SLIDE
-//     const targetSlide = slides[targetDotIndex];
-
-//     moveToSlide(carousel, currentSlide, targetSlide);
-//     toggleActive(currentDot, targetDot);
-//     hideButton(targetSlide, slides);
-// })
-
-// // MOVE TO DOT
-// function moveToDot(targetSlide, slides, nav, dots) {
-//     let slideIndex = findIndex(targetSlide, slides);
-//     const currentDot = nav.querySelector(".active");
-//     const targetDot = dots[slideIndex];
-//     toggleActive(currentDot, targetDot);
-// }
-// // MOVE TO SLIDE
-// function moveToSlide(carousel, currentSlide, targetSlide) {
-//     const position = targetSlide.style.left;
-//     carousel.style.transform = `translateX(-${position})`;
-//     toggleActive(currentSlide, targetSlide);
-// }
-
-// // Toggle ACTIVE CLASS
-// function toggleActive(current, target) {
-//     current.classList.remove("active");
-//     target.classList.add("active");
-// }
-
-// // HIDE BUTTON
-// function hideButton(targetSlide, slides) {
-//     // If the target slide is the first slide the previous button must be hidden
-//     // and the next button must be shown
-//     if (targetSlide === slides[0]) {
-//         previousButton.classList.add("carousel-btn-hide");
-//         nextButton.classList.remove("carousel-btn-hide");
-//         nextButton.classList.remove("carousel-btn-show");
-//         nextButton.classList.add("carousel-btn-show");
-
-//     } else if (targetSlide === slides[slides.length - 1]) {
-//         // If the target slide is the last slide the next button must be hidden
-//         // and the previous button must be shown
-//         nextButton.classList.add("carousel-btn-hide");
-//         previousButton.classList.remove("carousel-btn-hide");
-//         nextButton.classList.add("carousel-btn-show");
-//         // previousButton.classList.add("carousel-btn-show");
-//     } else {
-//         // if none of the above is true, we show both the next and prevoius button        
-//         previousButton.classList.remove("carousel-btn-hide");
-//         previousButton.classList.add("carousel-btn-show");
-//         nextButton.classList.add("carousel-btn-show");
-//         nextButton.classList.remove("carousel-btn-hide");
-//     }
-// }
 
 // FIND THE INDEX OF AN ITEM, INSIDE AN ARRAY OF ITEMS
 const projectsToggle = document.querySelectorAll(".toggle-projects");
@@ -533,9 +412,7 @@ function showSlides(n) {
     if (n < 1) {
         slideIndex = slides.length;
     }
-    /*for (i = 0; i < slides.length; i++) {
-	    slides[i].style.display = "none";  
-	}*/
+
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active-dot", "");
         slides[i].style.display = "none";
@@ -544,38 +421,20 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active-dot";
 }
 
-// setInterval(changeimg, 5000);
+// const learnMore = document.querySelectorAll(".learn-more");
+// const info = document.querySelectorAll(".slide-info p");
 
-// var currImg = 1;
-
-// function changeimg() {
-//     if (currImg == 1) {
-//         document
-//             .querySelector(".echallan-img")
-//             .setAttribute("src", "./media/files/echallan1.jpeg");
-//         currImg = 2;
-//     } else {
-//         document
-//             .querySelector(".echallan-img")
-//             .setAttribute("src", "./media/files/echallan_img.jpeg");
-//         currImg = 1;
-//     }
+// for (var i = 0; i < learnMore.length; i++) {
+//     learnMore[i].addEventListener("click", function () {
+//         if (this.innerHTML == "Show Less") {
+//             this.nextElementSibling.style.display = "none";
+//             this.innerHTML = "Learn More";
+//         } else {
+//             this.nextElementSibling.style.display = "block";
+//             this.innerHTML = "Show Less";
+//         }
+//     });
 // }
-
-const learnMore = document.querySelectorAll(".learn-more");
-const info = document.querySelectorAll(".slide-info p");
-
-for (var i = 0; i < learnMore.length; i++) {
-    learnMore[i].addEventListener("click", function () {
-        if (this.innerHTML == "Show Less") {
-            this.nextElementSibling.style.display = "none";
-            this.innerHTML = "Learn More";
-        } else {
-            this.nextElementSibling.style.display = "block";
-            this.innerHTML = "Show Less";
-        }
-    });
-}
 
 function findIndex(item, items) {
     for (let index = 0; index < items.length; index++) {
